216c55f0b98e2ebf2b5315055000831af4ad40ae
imtiaz-risat <ahababimtiaz@iut-dhaka.edu>
2024-07-03 21:02:35 +0600

    Footer added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,7 @@ import HeroSection from "../Components/HeroSection";
 import AboutSection from "../Components/AboutSection";
 import EventsSection from "../Components/EventsSection";
 import CollabSection from "../Components/CollabSection";
+import Footer from "../Components/Footer";
 
 export default function Home() {
   return (
@@ -13,6 +14,7 @@ export default function Home() {
       <AboutSection />
       <EventsSection />
       <CollabSection />
+      <Footer />
     </div>
   );
 }
